af4d2a5dac53541ebd3fe0b66fdd3df2c46a7d43
chengzhiyuan <chengzhiyuan@ktjr.com>
2017-01-11 14:07:05 +0800

    资产和资管详情页

--- app/views/pano/assets-manage/assets_manage.js
+++ app/views/pano/assets-manage/assets_manage.js
@@ -1,15 +1,39 @@
 ;
 (function() {
     'use strict';
-    angular.module('kt.pano').controller('ktAssetManageCtrl', function($scope, $rootScope, ktAessetsManageProductService, ktDataHelper) {
-        ktAessetsManageProductService.get({ id: 1 },
+    angular.module('kt.pano').controller('ktAssetManageCtrl', function($scope, $rootScope, ktProductsService, ktDataHelper, ktSweetAlert, $stateParams) {
+        var id = $stateParams.id
+        ktProductsService.get({ content: id },
             function(data) {
                 $scope.productManage = data.products
                 var inst = $scope.inst = data.from_info
                 if (!inst) return
                 inst.descObj = ktDataHelper.textEllipsis(inst.from_introduce, '.init-main-info .desc', 0, 14, 3, 6)
                 $scope.similars = data.similar_products
+                function groupData(arr) {
+                    if (arr.length % 3 !== 0) {
+                        arr.push({
+                            empty: true
+                        })
+                        groupData(arr)
+
+                    } else {
+                        $scope.termRates = arr
+                    }
+                }
+                console.log(data.products.term_rates)
+                groupData(data.products.term_rates)
 
             })
+             //弹出pano酱二维码
+        $scope.alertCode = function() {
+            ktSweetAlert.swal({
+                title: '<p class="alert">' + '更多产品数据，请联系微信客服PANO酱' + '</p>',
+                text: '<div class="img-pano">' + '<img src="../../../images/pano_wxSEC.png">' + '</div>',
+                html: true,
+                showCloseButton: true
+                // showCancelButton: true
+            })
+        }
     })
 })();
